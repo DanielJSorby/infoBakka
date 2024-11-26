f839b5c97512580c67a874fec5faac286acb164e
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2024-11-26 11:12:25 +0100

    Lagde kalender box, og font klasser

--- frontend/Info Bakka/src/components/calendar/box.svelte
+++ frontend/Info Bakka/src/components/calendar/box.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Box from './box.svelte';
     export let time = '12:15-13:15';
     export let fag = 'fag';
     export let color = '#FF5252'
@@ -7,15 +6,34 @@
 
 <div class="box" style="background-color: {color};">
     <div class="time-container">
-        <p>{time}</p>
+        <p class="regular">{time}</p>
     </div>
     <div class="fag-container">
-        <h2>{fag}</h2>
+        <h2 class="bold">{fag}</h2>
     </div>
 </div>
 
 <style>
     .box {
-        width: 100%;
+        width: 374px;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        border-radius: 20px;
+    }
+
+    .time-container {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+        font-size: 16px;
+        color: #E0E0E0;
+    }
+
+    .fag-container {
+        font-size: 36px;
+        color: #FFFFFF;
     }
 </style>
