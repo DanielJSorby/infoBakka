bed81ab6f3a9000f26bd96ae983df6bdcaec72a8
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2024-11-26 10:30:43 +0100

    La til global css og jobber med box

---
+++ frontend/Info Bakka/src/components/calendar/box.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Box from './box.svelte';
+    export let time = '12:15-13:15';
+    export let fag = 'fag';
+    export let color = '#FF5252'
+</script>
+
+<div class="box" style="background-color: {color};">
+    <div class="time-container">
+        <p>{time}</p>
+    </div>
+    <div class="fag-container">
+        <h2>{fag}</h2>
+    </div>
+</div>
+
+<style>
+    .box {
+        width: 100%;
+    }
+</style>
