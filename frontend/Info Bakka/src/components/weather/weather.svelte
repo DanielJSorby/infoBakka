5942222cc4d316bc9aa3fd37827c4a4fba3a21d8
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2024-11-26 13:01:25 +0100

    Fiksa kalender og vær på hjemmesiden

---
+++ frontend/Info Bakka/src/components/weather/weather.svelte
@@ -0,0 +1,53 @@
+<script>
+    export let weather = 'overskyet';
+    export let temperature = '17';
+    export let day = 'Onsdag'
+    export let date = '25'
+    export let month = 'Nov'
+
+</script>
+
+<div class="body {weather}">
+    <h2 class="semi-bold temperature">{temperature}</h2>
+    <p class="thin date">{day} {date}. {month}</p>
+</div>
+
+<style>
+    .overskyet {
+        background-image: url(/src/images/weather/overskyet.svg);
+    }
+
+    .skyet {
+        background-image: url(/src/images/weather/skyet.svg);
+    }
+
+    .sol {
+        background-image: url(/src/images/weather/sol.svg);
+    }
+
+    .regn {
+        background-image: url(/src/images/weather/regn.svg);
+    }
+
+    .body {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 260px;
+        width: 100%;
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+        color: #707070;
+    }
+
+    .temperature {
+        font-size: 64px;
+        padding-right: 100px;
+    }
+
+    .date {
+        font-size: 32px;
+    }
+</style>
--- frontend/Info Bakka/src/routes/+page.svelte
+++ frontend/Info Bakka/src/routes/+page.svelte
@@ -1,5 +1,40 @@
 <script>
     import CalendarBox from '../components/calendar/box.svelte'
+    import Weather from '../components/weather/weather.svelte'
 </script>
 
-<CalendarBox time='08:15-09:45' fag='Matte'/>
+<Weather weather='overskyet' temperature='17' day='Onsdag' date='25' month='Nov'/>
+
+<div class="calendar">
+    <h3 class="regular">Onsdag</h3>
+    <div class="line">Hei</div>
+    <CalendarBox fag='Norsk' time='08:15-09:45'/>
+    <div class="line">Hei</div>
+    <CalendarBox fag='Matte' time='10:00-11:30' color='#81C784'/>
+</div>
+
+<style>
+    .calendar {
+        width: 404px;
+        height: 276px;
+        overflow: hidden;
+        background-color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border-radius: 32px;
+    }
+
+    .calendar h3 {
+        color: #707070;
+        align-self: flex-start;
+        margin-left: 32px;
+        font-size: 28px;
+    }
+
+    .calendar .line {
+        width: 354px;
+    }
+</style>
